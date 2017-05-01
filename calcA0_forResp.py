171617013997b0f5a40a451a94043c44c06fa9c4
Kimberly Schramm <kschramm@usgs.gov>
2017-05-01 14:46:52 -0600

    script for calculation of the frequency associated with the pole from a response file

--- calcA0_forResp.py
+++ calcA0_forResp.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from obspy.signal.invsim import paz_to_freq_resp
+# this is from Austin Holland's seisutils
 from response import Response
 
 # for the STS 5/360 
@@ -11,19 +12,24 @@ from response import Response
 #instName='STS-5/360'
 
 # for the TR-360
-pol=[-0.01234+1j*0.01234, -0.01234-1j*0.01234, -196-1j*231, -196+1j*231, -732-1j*1450, -732+1j*1450, -173]
-zer=[0, 0, -108, -161]
-instName='TR-360'
+#pol=[-0.01234+1j*0.01234, -0.01234-1j*0.01234, -196-1j*231, -196+1j*231, -732-1j*1450, -732+1j*1450, -173]
+#zer=[0, 0, -108, -161]
+#instName='TR-360'
 
 #for the STS-1t5
-#pol=[-0.01234+1j*0.01234, -0.01234-1j*0.01234, -0.021995,  -0.026784, -39.18-1j*49.12, -39.18+1j*49.12]
-#zer=[0, 0, -0.02427184, -0.02427184]
-#instName='STS-1t5b5'
+#pol=[ -39.18-1j*49.12, -39.18+1j*49.12]
+#zer=[-0.02427184, -0.02427184]
+#instName='STS-1 Aaron'
+
+#for the STS-1
+##pol=[-0.01234+1j*0.01234, -0.01234-1j*0.01234, -39.18-1j*49.12, -39.18+1j*49.12]
+#zer=[0, 0]
+#instName='STS-1'
 
 #for the STS-6
-#pol=[-0.0123+1j*0.01206, -0.0123-1j*0.01206, -22.3068, -22.3068, -501.105, -501.105, -501.105, -501.105, -501.105, -501.105, -121.58+1j*647.231, -121.58-1j*647.231, -351.858]
-#zer=[0, 0, -21.991, -21.991, -351.858, -605.071, -521.5+1j*960.699, -521.5-1j*960.699]
-#instName='STS-6'
+pol=[-0.0123+1j*0.01206, -0.0123-1j*0.01206, -22.3068, -22.3068, -501.105, -501.105, -501.105, -501.105, -501.105, -501.105, -121.58+1j*647.231, -121.58-1j*647.231, -351.858]
+zer=[0, 0, -21.991, -21.991, -351.858, -605.071, -521.5+1j*960.699, -521.5-1j*960.699]
+instName='STS-6'
 
 #for the STS-6B
 #pol=[-0.01234-1j*0.01234, -0.01234+1j*0.01234, -255, -15.6, -97.3+1j*401, -97.3-1j*401, -520, -375, -13300, -10500-1j*10100, -10500+1j*10100]
@@ -34,7 +40,7 @@ instName='TR-360'
 inst = Response(desc=instName,units='Radians')
 inst.zeros = zer
 inst.poles = pol
-norm_freq=0.05
+norm_freq=0.02
 n,f = inst.check_normalization(freq=norm_freq, nfft=2**26,t_sample=0.001)
 scale_fac= 1.0/n
 print ('The A0 norm factor is: '+str(scale_fac)+' for f='+str(norm_freq))
@@ -42,7 +48,7 @@ print ('The A0 norm factor is: '+str(scale_fac)+' for f='+str(norm_freq))
 
 #poles = [-4.440 + 4.440j, -4.440 - 4.440j, -1.083 + 0.0j]
 #zeros = [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j]
-scale_fac = 0.4
+#scale_fac = 0.4
 
 h, f = paz_to_freq_resp(inst.poles, inst.zeros, scale_fac, 0.001, 2**26, freq=True)
 
